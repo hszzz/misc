f55d73dd76afd6b24e2fb905bef06a755db0a24c
hszzz <hszzz.me@gmail.com>
2021-05-07 21:37:16 +0800

    constexpr dunction

--- cpp11/constexpr/constexpr_function.cpp
+++ cpp11/constexpr/constexpr_function.cpp
@@ -6,8 +6,15 @@ constexpr int get_num() {
 	return 5;
 }
 
+constexpr int get_num1() {
+	constexpr int i = 1;
+	constexpr int j = 1;
+	return i + j;
+}
+
 int main() {
 	cout << get_num() << endl;
+	cout << get_num1() << endl;
 	return 0;
 }
 
