817de88f52c16bf3632b8ae7c39ed403d7a4a0f0
hszzz <hszzz.me@gmail.com>
2021-05-09 16:40:32 +0800

    add constexpr test

--- cpp11/constexpr/constexpr_function.cpp
+++ cpp11/constexpr/constexpr_function.cpp
@@ -6,15 +6,44 @@ constexpr int get_num() {
 	return 5;
 }
 
+// c++11 -> error; c++14 -> ok
 constexpr int get_num1() {
-	constexpr int i = 1;
-	constexpr int j = 1;
-	return i + j;
+	// constexpr int i = 1;
+	// constexpr int j = 1;
+	// return i + j;
+	return 2;
 }
+
+template<typename T>
+constexpr T get(T t) {
+	return t;
+}
+
+class Test {
+public:
+	constexpr Test(int i, int j) : i(i), j(j) {}
+
+	void print() {
+		cout << i << " " << j << endl;
+	}
+
+	int i;
+	int j;
+};
 
 int main() {
 	cout << get_num() << endl;
-	cout << get_num1() << endl;
+	// cout << get_num1() << endl;
+	
+	constexpr int i = 1;
+	cout << get(i) << endl; // constexpr function
+
+	int j = 1;
+	cout << get(j) << endl; // common function
+
+	constexpr Test t(1, 2);
+	t.print();
+
 	return 0;
 }
 
