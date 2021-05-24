3b5780de89800168c8b607a44bedfbd7f641d631
hszzz <hszzz.me@gmail.com>
2021-05-24 21:17:57 +0800

    add uniform initialization test

--- cpp11/initializer_list/uniform_initialization.cpp
+++ cpp11/initializer_list/uniform_initialization.cpp
@@ -2,7 +2,33 @@
 
 using namespace std;
 
+class Test {
+public:
+	Test(int a) : a(a) { }
+	void print() {
+		cout << a << endl;
+	}
+private:
+	int a;
+};
+
 int main() {
+	int a = 1;
+	int a1 = { 1 };
+	int a2{ 1 };
+	cout << a << endl;
+	cout << a1 << endl;
+	cout << a2 << endl;
+
+	Test t = 1;
+	Test t1 = { 2 };
+	Test t2{ 3 };
+	Test t3(3);
+	t.print();
+	t1.print();
+	t2.print();
+	t3.print();
+
 	return 0;
 }
 
