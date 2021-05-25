902bc5e18fc2ef85e958d7150c4b2a3882674938
hszzz <hszzz.me@gmail.com>
2021-05-25 21:00:34 +0800

    init lambda test

--- cpp11/range-for/range-for.cpp
+++ cpp11/range-for/range-for.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+vector<int> gv{1, 2, 3, 2, 1};
+
+vector<int> get_gv() {
+	cout << __FUNCTION__ << endl;
+	return gv;
+}
+
 int main() {
 	vector<int> vec{1, 2, 3, 3, 2, 1};
 	for (auto& v : vec) {
@@ -13,6 +20,12 @@ int main() {
 		cout << v << " ";
 	}
 	cout << endl;
+
+	for (auto v : get_gv()) { // only call get_gv() once
+		cout << v << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
 
