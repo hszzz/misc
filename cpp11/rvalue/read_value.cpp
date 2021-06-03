7ce804d79f12f6887a9c72fd18ddb766896e64c5
hszzz <hszzz.me@gmail.com>
2021-06-03 20:39:25 +0800

    add read_vlaue.cpp

---
+++ cpp11/rvalue/read_value.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class Test {
+public:
+	Test() : arr(new int[1000000]) {
+		cout << "constructor ..." << endl;
+	}
+
+	Test(const Test& t) : arr(new int[1000000]) {
+		cout << "copy constructor ..." << endl;
+		for (int i=0; i<1000000; i++) {
+			arr[i] = t.arr[i];
+		}
+	}
+
+	Test(Test&& t) : arr(t.arr) {
+		cout << "move constructor ..." << endl;
+		t.arr = nullptr;
+	}
+
+	~Test() {
+		cout << "destructor ..." << endl;
+		delete[] arr;
+	}
+private:
+	int* arr;
+};
+
+Test get_test() {
+	return Test();
+}
+
+int main() {
+	int a = 5;
+	int& b = a;
+
+	int&& c = 6;
+
+	cout << a << " " << b << " " << c << endl;
+
+	Test t = get_test();
+
+	return 0;
+}
+
