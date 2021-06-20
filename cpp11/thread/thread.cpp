09a76740f539f39bbd8f66f172dab47d711c55d2
hszzz <hszzz.me@gmail.com>
2021-06-20 19:47:05 +0800

    add thread test

--- cpp11/thread/thread.cpp
+++ cpp11/thread/thread.cpp
@@ -19,11 +19,29 @@ void func2() {
 	}
 }
 
+void func3() {
+	std::this_thread::sleep_for(std::chrono::seconds(2));
+	std::this_thread::yield();
+	std::cout << "id:" << std::this_thread::get_id() << " sleep 2 s." << std::endl;
+}
+
+void func4() {
+	std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(3));
+	std::cout << "id:" << std::this_thread::get_id() << " sleep 3 s." << std::endl;
+}
+
 int main() {
-	std::thread t(func1, 10);
-	std::thread t1(func2);
-	t.join();
-	t1.join();
+	// std::thread t(func1, 10);
+	// std::thread t1(func2);
+	// t.join();
+	// t1.join();
+	
+	std::thread t2(func3);
+	std::thread t3(func4);
+	t2.join();
+	t3.join();
+
+	std::cout << "main thread exited ." << std::endl;
 	return 0;
 }
 
