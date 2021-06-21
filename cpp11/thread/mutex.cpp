a67ec9c915e0db8517cc85e574a2debb80c8231c
hszzz <hszzz.me@gmail.com>
2021-06-21 20:51:03 +0800

    add mutex test

--- cpp11/thread/mutex.cpp
+++ cpp11/thread/mutex.cpp
@@ -2,10 +2,32 @@
 #include <mutex>
 #include <thread>
 
-using namespace std;
-using namespace thread;
+int num = 0;
+std::mutex mutex;
+
+void add() {
+	for (;;) {
+		mutex.lock();
+		num++;
+		mutex.unlock();
+		std::cout << "id:" << std::this_thread::get_id() << " add 1" << std::endl;
+	}
+}
+
+void print() {
+	for (;;) {
+		{
+			std::lock_guard<std::mutex> lock(mutex);
+			std::cout << "id:" << std::this_thread::get_id() << " num = " << num << std::endl;
+		}
+	}
+}
 
 int main() {
+	std::thread t1(add);
+	std::thread t2(print);
+	t1.join();
+	t2.join();
 	return 0;
 }
 
