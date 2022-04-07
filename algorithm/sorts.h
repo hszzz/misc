18525cd9788a7f90a1b8f404b5ea1f2470bced51
hszzz <hszzz.me@gmail.com>
2022-04-07 10:32:19 +0800

    heap_sort quick_sort insertion_sort selection_sort

---
+++ algorithm/sorts.h
@@ -0,0 +1,97 @@
+#include <functional>
+#include <iterator>
+
+namespace sort {
+
+namespace detail {
+
+template <typename Iter,
+          typename T = typename std::iterator_traits<Iter>::value_type,
+          typename Compare = std::less<T>>
+static Iter partition(Iter begin, Iter end, Compare cmp = Compare{}) {
+  const auto pv = *begin;
+  Iter piovt = begin;
+  for (auto it = std::next(begin, 1); it != end; ++it) {
+    if (cmp(*it, pv)) {
+      piovt = std::next(piovt, 1);
+      std::iter_swap(it, piovt);
+    }
+  }
+  std::iter_swap(begin, piovt);
+  return piovt;
+}
+
+template <typename Containter, typename T = typename Containter::value_type,
+          typename Compare = std::less<T>>
+static void make_heap(Containter& containter, size_t length, size_t index,
+                      Compare cmp = Compare{}) {
+  size_t largest = index;
+  size_t left = index * 2 + 1;
+  size_t right = index * 2 + 2;
+
+  if (left < length && cmp(containter[largest], containter[left]))
+    largest = left;
+  if (right < length && cmp(containter[largest], containter[right]))
+    largest = right;
+
+  if (largest != index) {
+    std::swap(containter[largest], containter[index]);
+    make_heap(containter, length, largest);
+  }
+}
+
+}  // namespace detail
+
+template <typename Iter,
+          typename T = typename std::iterator_traits<Iter>::value_type,
+          typename Compare = std::less<T>>
+static void insertion_sort(Iter begin, Iter end, Compare cmp = Compare{}) {
+  if (std::distance(begin, end) <= 1) return;
+
+  for (Iter it = begin + 1; it != end; ++it) {
+    for (Iter itt = it; std::distance(begin, itt) > 0; --itt) {
+      if (cmp(*itt, *(std::next(itt, -1))))
+        std::iter_swap(itt, std::next(itt, -1));
+    }
+  }
+}
+
+template <typename Iter,
+          typename T = typename std::iterator_traits<Iter>::value_type,
+          typename Compare = std::less<T>>
+static void section_sort(Iter begin, Iter end, Compare cmp = Compare{}) {
+  if (std::distance(begin, end) <= 1) return;
+
+  for (Iter it = begin; it != std::next(end, -1); ++it) {
+    auto memo = it;
+    for (Iter itt = std::next(it, 1); itt != end; ++itt) {
+      if (cmp(*itt, *memo)) memo = itt;
+    }
+    if (memo != it) std::iter_swap(it, memo);
+  }
+}
+
+template <typename Iter,
+          typename T = typename std::iterator_traits<Iter>::value_type,
+          typename Compare = std::less<T>>
+static void quick_sort(Iter begin, Iter end, Compare cmp = Compare{}) {
+  if (std::distance(begin, end) <= 1) return;
+  Iter piovt = detail::partition(begin, end, cmp);
+  quick_sort(begin, piovt, cmp);
+  quick_sort(piovt + 1, end, cmp);
+}
+
+template <typename Containter, typename T = typename Containter::value_type,
+          typename Compare = std::less<T>>
+static void heap_sort(Containter& containter, Compare cmp = Compare{}) {
+  for (int i = containter.size() / 2 - 1; i >= 0; --i) {
+    detail::make_heap(containter, containter.size(), i, cmp);
+  }
+
+  for (size_t i = containter.size() - 1; i > 0; --i) {
+    std::swap(containter[i], containter[0]);
+    detail::make_heap(containter, i, 0, cmp);
+  }
+}
+
+}  // namespace sort
---
+++ algorithm/test.cc
@@ -0,0 +1,45 @@
+#include <cassert>
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+#include "sorts.h"
+
+void debug(const std::vector<int>& array) {
+  for (const auto& item : array) std::cout << item << " ";
+  std::cout << std::endl;
+}
+
+std::vector<int> generate_random_vector(const int min, const int max,
+                                        const size_t n, const int seed = 0) {
+  assert(n > 0);
+
+  std::vector<int> res(n);
+  srand(seed);
+  for (size_t i = 0; i < n; ++i) {
+    res[i] = rand() % (max - min) + min;
+  }
+
+  return res;
+}
+
+template <typename Func>
+void benchmark(const Func& func) {
+  auto t0 = std::chrono::steady_clock::now();
+  func();
+  auto t1 = std::chrono::steady_clock::now();
+  auto dt = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+  std::cout << "time elapsed: " << dt.count() << std::endl;
+}
+
+int main() {
+  std::vector<int> array = generate_random_vector(0, 100, 20);
+  debug(array);
+  sort::section_sort(array.begin(), array.end());
+  debug(array);
+  sort::section_sort(array.begin(), array.end(), std::greater<>());
+  // benchmark([&array]() { sort::insertion_sort(array.begin(), array.end());
+  // });
+  debug(array);
+  return 0;
+}
